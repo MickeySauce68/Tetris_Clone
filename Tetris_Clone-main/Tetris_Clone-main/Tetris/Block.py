419fbaa806ab3e7a9d1dd95fe3b810499a4f0e03
MickeySauce68 <103530779+MickeySauce68@users.noreply.github.com>
2023-11-01 00:07:20 -0400

    Delete Tetris_Clone-main/Tetris_Clone-main/Tetris directory

--- Tetris_Clone-main/Tetris_Clone-main/Tetris/Block.py
+++
@@ -1,7 +0,0 @@
-class Block:
-    def __init__(self, id):
-        self.id = id    
-        self.cells = {}                                               
-        self.cells_size = 30
-        self.rotation_state = 0
-
